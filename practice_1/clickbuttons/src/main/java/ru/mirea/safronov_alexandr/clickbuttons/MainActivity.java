8f3b947b4e647d41c4bdd441e4cd4571b2beaee9
kemosaaabe <sanchellos27@mail.ru>
2022-02-23 21:02:15 +0300

    W

---
+++ practice_1/clickbuttons/src/main/java/ru/mirea/safronov_alexandr/clickbuttons/MainActivity.java
@@ -0,0 +1,40 @@
+package ru.mirea.safronov_alexandr.clickbuttons;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView tvOut;
+    private Button btnOk;
+    private Button btnCancel;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tvOut = (TextView) findViewById(R.id.tvOut);
+        btnOk = (Button) findViewById(R.id.btnOk);
+        btnCancel = (Button) findViewById(R.id.btnCancel);
+
+        View.OnClickListener oclBtnOk = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tvOut.setText("Нажата кнопка OK");
+            }
+        };
+
+        View.OnClickListener oclBtnCancel = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tvOut.setText("Нажата кнопка Cancel");
+            }
+        };
+
+        btnOk.setOnClickListener(oclBtnOk);
+        btnCancel.setOnClickListener(oclBtnCancel);
+    }
+
+}
---
+++ practice_1/layouttype/src/main/java/ru/mirea/safronov_alexandr/layouttype/MainActivity.java
@@ -0,0 +1,14 @@
+package ru.mirea.safronov_alexandr.layouttype;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.constraint_layout);
+    }
+}
