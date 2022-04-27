7198d17315a156405d958387759df2b6ba82da8f
kemosaaabe <sanchellos27@mail.ru>
2022-04-27 22:38:08 +0300

    5

---
+++ practice_5/MireaProject/simplefragmentapp/src/main/java/ru/mirea/safronov_alexandr/simplefragmentapp/SensorsFragment.java
@@ -0,0 +1,145 @@
+package ru.mirea.safronov_alexandr.simplefragmentapp;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link SensorsFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class SensorsFragment extends Fragment implements SensorEventListener {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public SensorsFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment fragment_sensors.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static SensorsFragment newInstance(String param1, String param2) {
+        SensorsFragment fragment = new SensorsFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    private TextView azimuthTextView;
+    private TextView pitchTextView;
+    private TextView rollTextView;
+    private TextView magneticTextView;
+    private TextView gyroTextView;
+
+    private SensorManager sensorManager;
+    private Sensor accelerometerSensor;
+    private Sensor magneticSensor;
+    private Sensor gyroSensor;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View view =inflater.inflate(R.layout.fragment_sensors, container, false);
+
+        sensorManager = (SensorManager)getActivity().getSystemService(Context.SENSOR_SERVICE);
+
+        accelerometerSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        gyroSensor = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+        magneticSensor = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+
+        sensorManager.registerListener(this, accelerometerSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(this, magneticSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(this, gyroSensor, SensorManager.SENSOR_DELAY_NORMAL);
+
+        azimuthTextView = view.findViewById(R.id.azimuth);
+        pitchTextView = view.findViewById(R.id.pitch);
+        rollTextView = view.findViewById(R.id.roll);
+
+        magneticTextView = view.findViewById(R.id.magnetic);
+
+        gyroTextView = view.findViewById(R.id.gyroscope);
+
+        return view;
+    }
+
+    public void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, accelerometerSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(this, magneticSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(this, gyroSensor, SensorManager.SENSOR_DELAY_NORMAL);
+
+    }
+
+
+
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            float valueAzimuth = event.values[0];
+            float valuePitch = event.values[1];
+            float valueRoll = event.values[2];
+            azimuthTextView.setText("Azimuth: " + valueAzimuth);
+            pitchTextView.setText("Pitch: " + valuePitch);
+            rollTextView.setText("Roll: " + valueRoll);
+        }
+
+        else if (event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) {
+            float valueMagnetic = event.values[0];
+            magneticTextView.setText("Magnetic Field: " + valueMagnetic);
+        }
+
+        else if (event.sensor.getType() == Sensor.TYPE_GYROSCOPE) {
+            float value = event.values[0];
+            gyroTextView.setText("Gyroscope: " + value);
+        }
+    }
+
+
+
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+}
